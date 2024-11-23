b5bd728b4eeb46b74997d2848dcb1f4293b86c6c
pankaj Yadav <pankajyadav851685@gmail.com>
2024-11-23 10:39:32 +0530

    conditions

---
+++ index3.js
@@ -0,0 +1,40 @@
+console.log("this our 56 video tutorial");
+// let number = prompt()
+// number = Number(number);
+// console.log("This is number: "+number)
+
+// let age = 17;
+// let grace =2;
+// console.log('====================================');
+// console.log(age+grace);
+// console.log('====================================');
+// if((age+grace)>18){
+//     console.log("you can Drive")
+// }
+// else{
+//     console.log("YOU Can't Drive")
+// }
+
+
+// ============ Arithmatic Operator =========
+
+let age = 3;
+let grace = 2;
+
+console.log(age+grace);
+console.log(age-grace);
+console.log(age*grace);
+console.log(age/grace);
+console.log(age%grace);
+console.log(age**grace);// exponensial operator
+
+
+// baki chije theory hai code with harry notes se padna hai . 
+
+// ternary operator
+let a= 5
+let b=8;
+c=a>b ? (a-b):(b-a)
+console.log('====================================');
+console.log(c);
+console.log('====================================');
