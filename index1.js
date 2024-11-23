9267df095356d8b872c657c4a66986787b4e4867
pankaj Yadav <pankajyadav851685@gmail.com>
2024-11-23 09:57:51 +0530

    js

---
+++ index1.js
@@ -0,0 +1,8 @@
+alert("Hello World!")
+console.log("Hello pankaj!! ");
+console.log("kese ho  ");
+console.log("i hope ki badiya hoge. ");
+
+var a= prompt("enter your number ");
+console.log("Your number is " + a);
+
---
+++ index2.js
@@ -0,0 +1,40 @@
+console.log("Hey this is tutorial 55");
+
+// var a = 5;
+// var b = 6;
+// var c= "Harry";
+// var _a= "Shubham";
+let a = 5;
+let b = 6;
+let c= "Harry";
+let _a= "Shubham";
+// var 55a = "Rohan"; // not allowed
+
+// console.log(a+b + 8);
+// console.log(typeof a, typeof b, typeof c, typeof _a);
+
+const a1 = 6;
+// a1 = a1 +1; // not allowed
+
+{ // block
+let a = 66;
+    // console.log(a);
+
+    //  let ka block scope hota hai , var ka global scope hota hai 
+    
+
+}
+
+// console.log(a );
+
+
+let o = {
+    "name": "Pankaj",
+    "job code": 56000,
+    "is handasome": true
+}
+
+console.log(o);
+o.salary = "100crores"
+console.log(o);
+
