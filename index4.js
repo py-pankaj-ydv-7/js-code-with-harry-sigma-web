040566c361b746511e9c18ef6c8ef5d874cf253a
pankaj Yadav <pankajyadav851685@gmail.com>
2024-11-23 13:03:41 +0530

    loops

---
+++ index4.js
@@ -0,0 +1,48 @@
+
+console.log(" I am a tutorial on Loops");
+let a = 1;
+//  for(i = 0;i<100;i++){
+    
+    // console.log(a+i);
+    
+//  }
+
+ let obj={
+    name: "Pankaj Yadav",
+    role: "software Developer",
+    company: "ACCio",
+    salary: "50000$"
+ }
+//  console.log(obj);
+ 
+
+ // for in loop
+
+ for (const key in obj) {
+      
+    const element = obj[key];
+    // console.log(key,element);
+    
+ }
+
+ // for of loop
+
+ for (const c of "Harry"){
+    // console.log(c);
+    
+ }
+
+//while loop
+
+let i = 0;
+while(i<60000){
+    // console.log(i);
+    i++;    
+}
+
+let p = 0;
+do{
+    console.log(p);
+    p++;  
+}while(p<10)
+
