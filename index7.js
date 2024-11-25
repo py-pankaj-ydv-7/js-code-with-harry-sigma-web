5b57539e6522206b03304b0ed7388d379e4fd789
Pankaj Yadav <pankajyadav851685@gmail.com>
2024-11-25 18:37:24 +0530

    faulty calculator

---
+++ index7.js
@@ -0,0 +1,15 @@
+/**
+Create a faulty calculator using javascript 
+
+This faulty calculator does following:
+
+1.It takes two numbers as input from the user
+2.It performs wrong operations as follows:
+
++ ----> -
+* ----> +
+- ----> /
+/ ----> **
+It perform wrong operation 10% of the times
+
+ */
