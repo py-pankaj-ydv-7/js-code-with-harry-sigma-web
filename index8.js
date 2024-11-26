94b4fc80fc4344083e85c1d57067fdb0b136e515
Pankaj Yadav <pankajyadav851685@gmail.com>
2024-11-26 06:24:54 +0530

    complete strings

--- index8.js
+++ index8.js
@@ -1,5 +1,5 @@
 console.log("This is strings tutorial ");
-let a = "Harry";
+let a = " Harry ";
 
 console.log(a[0]);
 console.log(a[1]);
@@ -13,6 +13,21 @@ let real_name = "Harry"
 let friend = "Rohan"
 console.log("His name is "+ real_name+ "and his frinds name is "+ friend);
 console.log(`his name is ${real_name} and his friends name is ${friend}`); // ye template literals
+
+let b = "pankaj"
+console.log(b.toLowerCase());
+console.log(b.toUpperCase());// its function to convert charactor lowercash and uppercash
+console.log(b.length); // its a property
+console.log(b.slice(1,3)); // 3 is not included
+console.log(b.replace("pa","77"));
+console.log(b);
+
+console.log(b.concat(a," Aishwaiya "));
+
+
+// string is a immutable 
+
+
 
 
 
