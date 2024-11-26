ec56bf0720d01e98548bf265a5e77d8076b0fb6d
Pankaj Yadav <pankajyadav851685@gmail.com>
2024-11-26 06:05:42 +0530

    litrals in string

---
+++ index8.js
@@ -0,0 +1,19 @@
+console.log("This is strings tutorial ");
+let a = "Harry";
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+console.log(a[5]); /// ye undefined dega .
+
+let real_name = "Harry"
+let friend = "Rohan"
+console.log("His name is "+ real_name+ "and his frinds name is "+ friend);
+console.log(`his name is ${real_name} and his friends name is ${friend}`); // ye template literals
+
+
+
+
