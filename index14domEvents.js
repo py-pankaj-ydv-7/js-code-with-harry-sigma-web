2af9039fc9847faf33e753bfe1a96bcbff72b404
Pankaj Yadav <pankajyadav851685@gmail.com>
2024-11-27 10:46:45 +0530

    events

---
+++ index14domEvents.js
@@ -0,0 +1,11 @@
+let button = document.getElementById("btn")
+
+button.addEventListener("click", ()=>{
+    // console.log("hlo pankaj"); // console me print hoga 
+    document.querySelector(".box").innerHTML = "hello pankaj"
+
+    // alert("i was clicked")
+    
+})
+
+// there  are many events  , mouse events and keyboard events , their are so much events 
