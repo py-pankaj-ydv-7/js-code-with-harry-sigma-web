3a87673cd4aaf441a63dc54361206c849039077a
Pankaj Yadav <pankajyadav851685@gmail.com>
2024-11-27 08:14:47 +0530

    inserting and removing element

---
+++ index13dom.js
@@ -0,0 +1,4 @@
+let div = document.createElement("div");
+div.innerHTML = "i have been inseted <b>by pankaj</b>"
+div.setAttribute("class", "created");
+document.querySelector(".container").append(div)
